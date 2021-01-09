5ec91950815208be7c50436a21c77411984958ae
Jennifer Cribbs <cribbs.jennifer@gmail.com>
2021-01-09 15:30:15 -0600

    Added ToString

--- Assets/Scripts/GameStatus.cs
+++ Assets/Scripts/GameStatus.cs
@@ -1,15 +1,22 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class GameStatus : MonoBehaviour
 {
     // Config Params
     [Range(0.1f, 10f)] [SerializeField] float gameSpeed = 1f;
     [SerializeField] int pointsPerBlockDestroyed = 83;
+    [SerializeField] TextMeshProUGUI scoreText;
 
     //State variables
     [SerializeField] int CurrentScore = 0;
+
+    private void Start()
+    {
+        scoreText.text = CurrentScore.ToString();     
+    }
 
     // Update is called once per frame
     void Update()
@@ -20,6 +27,7 @@ public class GameStatus : MonoBehaviour
     public void addToScore()  
     {
         CurrentScore += pointsPerBlockDestroyed;
+        scoreText.text = CurrentScore.ToString();
     }
     
 }
